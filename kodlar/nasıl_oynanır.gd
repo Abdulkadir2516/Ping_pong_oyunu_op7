c0c7b58326879d606013e098d1135776b26a131e
Abdulkadir Yeşilkaya <abdulkadir7725@gmail.com>
2025-05-24 21:25:14 +0300

    naasıl oynanır sayfası tamamlandı

--- kodlar/nasıl_oynanır.gd
+++ kodlar/nasıl_oynanır.gd
@@ -1,24 +1,33 @@
 extends ColorRect
 
-func _on_text_edit_gui_input(event: InputEvent) -> void:
+
+func _tus_atama(event: InputEvent, action_name: String, texedit: TextEdit):
+	
 	if event is InputEventKey and event.pressed:
 		var keycode = event.physical_keycode
 		var key_name = OS.get_keycode_string(keycode)
-
-		# Tuşun adını yazdır (debug)
-		print("Basilan tuş: ", key_name)
-
-		# Girdi haritasına ekle (örnek aksiyon: "my_action")
-		var action_name = "yukarı git"
-
-		if not InputMap.has_action(action_name):
-			InputMap.add_action(action_name)
+		
+		InputMap.action_erase_events(action_name)
 
 		var new_event = InputEventKey.new()
 		new_event.physical_keycode = keycode
+		
 		InputMap.action_add_event(action_name, new_event)
-
+		texedit.text = str(key_name)
+		
 
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://sahneler/giris.tscn")
+
+func _on_text_edit_gui_input(event: InputEvent) -> void:
+	_tus_atama(event, "yukarı git", $Label/TextEdit)
+
+func _on_text_edit_3_gui_input(event: InputEvent) -> void:
+	_tus_atama(event, "up", $Label2/TextEdit3)
+
+func _on_text_edit_2_gui_input(event: InputEvent) -> void:
+	_tus_atama(event, "aşağı git", $Label/TextEdit2)
+
+func _on_text_edit_4_gui_input(event: InputEvent) -> void:
+	_tus_atama(event, "down", $Label2/TextEdit4)
